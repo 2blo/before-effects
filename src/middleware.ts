1f96a3e4e4843ec5581bc3cb2f79adbc86e2ee25
2blo <2blo.dev@gmail.com>
2022-12-17 14:51:34 +0100

    Rate limit API endpoints 20 per 20 sec.
    * Does not redirect properly, but only matters for bots.
    * Requests are delayed, but still complete.
    
    Signed-off-by: 2blo <2blo.dev@gmail.com>

---
+++ src/middleware.ts
@@ -0,0 +1,37 @@
+import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
+import { Ratelimit } from "@upstash/ratelimit";
+import { Redis } from "@upstash/redis";
+
+const ratelimit = new Ratelimit({
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.fixedWindow(20, "20 s"),
+});
+
+export default async function middleware(
+  request: NextRequest,
+  event: NextFetchEvent
+) {
+  const ip = request.ip ?? "127.0.0.1";
+
+  if (request.nextUrl.pathname === "/api/blocked") {
+    return NextResponse.next();
+  }
+
+  const { success, pending, limit, reset, remaining } = await ratelimit.limit(
+    `mw_${ip}`
+  );
+  event.waitUntil(pending);
+
+  const res = success
+    ? NextResponse.next()
+    : NextResponse.redirect(new URL("/api/blocked", request.url), request);
+
+  res.headers.set("X-RateLimit-Limit", limit.toString());
+  res.headers.set("X-RateLimit-Remaining", remaining.toString());
+  res.headers.set("X-RateLimit-Reset", reset.toString());
+  return res;
+}
+
+export const config = {
+  matcher: ["/api/:path*"],
+};
