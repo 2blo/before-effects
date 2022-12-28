721fb0037995fbc1cb746f43fb93dec49e8e2685
2blo <2blo.dev@gmail.com>
2022-12-28 17:26:10 +0100

    Add lorem ipsum and Fix post spacing.
    * cant use li cause different seeds on ssr and csr.
    * increase spacing
    
    Signed-off-by: 2blo <2blo.dev@gmail.com>

--- src/pages/post/[id].tsx
+++ src/pages/post/[id].tsx
@@ -6,6 +6,19 @@ import { useRouter } from "next/router";
 import { aspectAtom } from "@ui/SplitScreen";
 import { useAtom } from "jotai";
 import Image from "next/image";
+import { LoremIpsum } from "lorem-ipsum";
+
+const lorem = new LoremIpsum({
+  sentencesPerParagraph: {
+    max: 8,
+    min: 4,
+  },
+  wordsPerSentence: {
+    max: 16,
+    min: 4,
+  },
+  random: () => 0.2,
+});
 
 const Video: NextPage = () => {
   const router = useRouter();
@@ -17,7 +30,6 @@ const Video: NextPage = () => {
   //     userQuery = trpc.user.getById.useQuery(id);
   //   }
   const [aspect] = useAtom(aspectAtom);
-  const user = "qguser";
   return (
     <div className="flex-col -space-y-16">
       <Splitscreen
@@ -32,9 +44,12 @@ const Video: NextPage = () => {
         after="https://preview.redd.it/fcwt76c4kp7a1.png?width=8000&format=png&auto=webp&s=fa00ec8bcb0979c26c078f6f9764c7a35a9db33a"
         before="https://preview.redd.it/h93al5c4kp7a1.png?width=8000&format=png&auto=webp&s=c35051f26e05d7a9d5a7cdf760f6cd1cb005035e"
       /> */}
-      <div className="mx-auto " style={{ width: aspect.width }}>
-        <h1>Title</h1>
-        <div className="flex">
+      <div
+        className="mx-auto flex-col space-y-2"
+        style={{ width: aspect.width }}
+      >
+        <h1>{"Long Video Title"}</h1>
+        <div className="flex gap-2">
           <Image
             className="h-16 w-16 rounded-full object-cover"
             src="/lifeform.jpg"
@@ -42,13 +57,11 @@ const Video: NextPage = () => {
             width={1000}
             height={1000}
           />
-          <h1 style={{ alignSelf: "flex-end" }}>{user}</h1>
+          <h1 style={{ alignSelf: "flex-end" }}>{"User name"}</h1>
         </div>
-        <p className="pt-4">
-          Description asdasda sdasdasdas da as dasd da od aoda oasdaosdmas dmas
-          dopmasopmdmas mdadas das dopasopda msodmoasodm moasdmop aopsd
-          mopasdmopas mopdaopasdasopdopasd poaopsd maspdm apsmdp amsdpm
-        </p>
+        <div className="whitespace-pre-wrap pt-8">
+          {lorem.generateParagraphs(4).replaceAll("\n", "\n\n")}
+        </div>
       </div>
     </div>
   );
