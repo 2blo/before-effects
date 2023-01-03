cdbf254e8bc89ed07773ce8063b6569071ea71c0
Isak Schwartz <33279671+isaksc2@users.noreply.github.com>
2023-01-03 19:49:24 +0100

    Add query to get thumbnail WIP
    * change workstation
    
    Signed-off-by: Isak Schwartz <33279671+isaksc2@users.noreply.github.com>

--- src/pages/user/[id].tsx
+++ src/pages/user/[id].tsx
@@ -2,6 +2,61 @@ import { useRouter } from "next/router";
 import { type NextPage } from "next";
 import { trpc } from "../../utils/trpc";
 import NextError from "next/error";
+import Image from "next/image";
+import { Post, Content } from "@prisma/client";
+import axios from "axios";
+import {
+  useQuery,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import { z } from "zod";
+
+const queryClient = new QueryClient();
+
+function useYoutubeThumbnail(after: Post["after"]) {
+  return useQuery({
+    queryKey: ["thumbnails", after],
+    queryFn: async () => {
+      const { data } = await axios.get(
+        "https://jsonplaceholder.typicode.com/posts"
+      );
+      return data;
+    },
+  });
+}
+
+export interface ThumbnailProps
+  extends React.HTMLAttributes<HTMLDivElement>
+     {
+  contenttype: Content;
+  title: Post.["title"];
+  createdat: Post["createdAt"];
+  after: Post["after"];
+}
+
+const Thumbnail: React.FC<ThumbnailProps> = ({
+  className,
+  ...props
+}) => {
+  let image: string
+  if (props.contenttype === Content.IMAGE) {
+    image = props.after
+  } else {
+    https://www.googleapis.com/youtube/v3/videos?key=YOUR_API_KEY&part=snippet&id=VIDEO_ID
+  }
+  return (
+    <QueryClientProvider client={queryClient}>
+
+    <div>
+      <Image className="object-cover h-28 w-[119.111px]" src={props.after} alt="alt" width={400} height={300}></Image>
+      <h3>{props.title}</h3>
+      <h4>{props.createdat}</h4>
+    </div>
+    </QueryClientProvider>
+  )
+}
 
 const UserPage: NextPage = () => {
   // const userId = useRouter().query.id as string;
@@ -46,6 +101,7 @@ const UserPage: NextPage = () => {
         <div className="h-32 w-32 bg-red-400"></div>
         <div className="h-32 w-32 bg-red-400"></div>
         <div className="h-32 w-32 bg-red-400"></div>
+        <Thumbnail title={"title lol"} createdat={"2022-21-34"} after={"/lifeform.jpg"} contenttype={Content.IMAGE}></Thumbnail>
       </div>
     </div>
   );
