2a114defd010c3c4b8d7fa255e001ea0d0c84d6d
2blo <2blo.dev@gmail.com>
2023-02-16 22:44:38 +0100

    Add opaque button hocus style
    * no feedback before
    
    Signed-off-by: 2blo <2blo.dev@gmail.com>

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -21,7 +21,10 @@ const button = cva("button", {
         base,
         "bg-gradient-to-r from-black/50 to-neutral-900/50 border-black/30  hocus:from-neutral-600/40",
       ],
-      opaque: [base, "bg-gradient-to-r from-red-700 to-rose-600 text-white"],
+      opaque: [
+        base,
+        "bg-gradient-to-r from-red-700 to-rose-600 text-white hocus:from-red-600 hocus:to-rose-500",
+      ],
       inList: ["group flex w-full items-center rounded-md px-2 py-2 text-sm"],
     },
     size: {
